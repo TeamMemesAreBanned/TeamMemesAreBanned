0ae4efc3245089dcde92810032c131d52b278b37
Ian Robinson <irobinson@bigvikinggames.com>
2018-06-22 15:44:05 -0400

    add in tilemaps and game objective

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour {
+
+    public bool trophyCollected = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
